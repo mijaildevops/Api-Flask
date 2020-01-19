774eff87845de840d55f7a919fe8575c194feb45
mosorio <mijailosorioh@gmail.com>
2020-01-19 12:06:54 -0500

    Comentatios Por bloque de codigo

--- app.py
+++ app.py
@@ -26,24 +26,36 @@ def getdata():
     
     cur = mysql.connection.cursor()
     cur.execute('SELECT name, Continent, Region, HeadOfState FROM country')
+    # la variable data es una lista, cada registro encontrado en la DB es una elemento de la lista 
+    # un elemento de la lista puede tener N numero de propiedades (Columnas SELECT)
     data = cur.fetchall()
     print (json.dumps(data))
-    #return jsonify (data)
-    #print (json.dumps(data))
+
 
     return jsonify(
-        # esto es un arrecglo de Ob
+        # //////////////////////////////////////////////////////////////
         # un objeto json esta represantado entre llaves, todo lo que esta dentro de las llaves son propiedades del objeto 1
-        # para selecionar 
-        {"Data": data[0][1], "message": "Lista de db"},
-        {"Data": data[1], "message": "Lista de db"},
-        {"Data": data[2], "message": "Lista de db"},
-        {"Data": data[3], "message": "Lista de db"},
-        {"Data": data[4], "message": "Lista de db"}
+        # //////////////////////////////////////////////////////////////
+
+        # //////////////////////////////////////////////////////////////
+        # De esta forma se imprimer el primer Elemento o registro de la Lista con todas sus propiedades
+        {"Paises": data[0], "message": "Forma de imprimir el primer Elemento o registro de la Lista con todas sus propiedades"},
+
+        # //////////////////////////////////////////////////////////////
+        # De esta forma se imprimer el primer Elemento o registro de la Lista, seleccionado solo una Propieded
+        {"Pais": data[0][0], "message": "Forma de imprimir el primer Elemento o registro de la Lista, seleccionado solo una Propiedad"},
+        
+        # //////////////////////////////////////////////////////////////
+        # De esta forma se Imprimen todos los registros
+        {"Data": data, "message": "Forma de Imprimin todos los registros o Elementos"}
         )
     
 # //////////////////////////////////////////////////////////////
 # Setting Main Funtions
+# configuracion de ambiente para hacer Deploy
+# Setting Host dende se publica
+# Setting Port
+# Setting Debug Opcion, para que cada vez que se hace save Aplique un reload
 # ////////////////////////////////////////////////////////////// 
 if __name__ == '__main__':
      app.run(port = 4321, debug = True, host='192.168.100.233') # PC Main
