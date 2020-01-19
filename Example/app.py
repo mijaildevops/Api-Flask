8c11e336d7f7b20170093f82205567cbae14053b
mosorio <mijailosorioh@gmail.com>
2020-01-19 11:40:59 -0500

    Carpeta Example json Api

--- Example/app.py
+++ Example/app.py
@@ -16,32 +16,43 @@ app.config ['MYSQL_DB'] = 'world'
 mysql = MySQL(app)
 
 # //////////////////////////////////////////////////////////////
+# Get Data desde archivo Externo "products.py"
+# //////////////////////////////////////////////////////////////
+from products import products
+
+# //////////////////////////////////////////////////////////////
 # Setting Rutas
 # ////////////////////////////////////////////////////////////// 
 
 # //////////////////////////////////
-@app.route ('/data')
+@app.route ('/Data')
 def getdata():
-    #return 'Hello word'
-    
     cur = mysql.connection.cursor()
-    cur.execute('SELECT name, Continent, Region, HeadOfState FROM country')
+    cur.execute('SELECT * FROM performance order by id desc LIMIT 5')
     data = cur.fetchall()
     print (json.dumps(data))
-    #return jsonify (data)
-    #print (json.dumps(data))
-
+    
+    #datas = (json.dumps(data))
     return jsonify(
-        {"Data": data[0][1], "message": "Lista de db"},
+        {"Data": data[0], "message": "Lista de db"},
         {"Data": data[1], "message": "Lista de db"},
         {"Data": data[2], "message": "Lista de db"},
         {"Data": data[3], "message": "Lista de db"},
         {"Data": data[4], "message": "Lista de db"}
         )
     
+# //////////////////////////////////
+@app.route('/test')
+def test():
+    return 'Pagina test API-REST'
+
+# //////////////////////////////////
+@app.route ('/products')
+def getproducts():
+    return jsonify({"products": products, "message": "Lista de Productos"})
+
 # //////////////////////////////////////////////////////////////
 # Setting Main Funtions
 # ////////////////////////////////////////////////////////////// 
 if __name__ == '__main__':
-     app.run(port = 4321, debug = True, host='192.168.100.233') # PC Main
-    # app.run(port = 4321, debug = True, host='192.168.100.156') # Laptop
+    app.run(debug=True, port=4320)
---
+++ Example/products.py
@@ -0,0 +1,5 @@
+products = [
+    {"name":  "Laptop", "Price" : 842, "Quantiy": 4},
+    {"name":  "Monitor", "Price" : 154, "Quantiy": 8},
+    {"name":  "Mouse", "Price" : 20, "Quantiy": 12}
+]
