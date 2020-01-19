0d50be00465231084a6d80ff154b3faae0b5fd81
mijail devops <mijail.devops@gmail.com>
2020-01-19 08:02:13 -0800

    Conexion a la DB

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = '192.168.100.51'
+app.config['MYSQL_USER'] = 'Qatest'
+app.config['MYSQL_PASSWORD'] = 'Quito.2019'
+app.config['MYSQL_DB'] = 'Automatizacion'
+mysql = MySQL(app)
+
 
 @app.route ('/')
 def Index():
-    return 'Hello word'
+    #return 'Hello word'
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM record')
+    data = cur.fetchall()
+    return jsonify (data[0])
 
 
 if __name__ == '__main__':
